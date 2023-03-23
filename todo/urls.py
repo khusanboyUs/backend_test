3769e52cbf654440c96d016be901fd40391200cf
khusanboyUs <khusanjon.usmonov@globallogic.com>
2023-03-23 21:34:45 +0100

    created new Index Function

--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def IndexPage(request):
+    return HttpResponse('Hello world')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexPage,name='index'),
 ]
